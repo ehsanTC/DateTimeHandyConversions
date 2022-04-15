1ea12ab7790c387ff0f271483272e2f5a2c3f4ff
Ehsan Toghian <ehsan_toghian@yahoo.com>
2022-04-15 17:43:59 +0430

    Initial commit

---
+++ DateExtensions.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using TimeZoneConverter;
+
+namespace Doctors.Service.Extensions
+{
+    public static class DateExtensions
+    {
+        /// <summary>
+        /// Sets time for a date. The previous time will be cleared.
+        /// </summary>
+        /// <param name="baseDate">The date which its time will be set.</param>
+        /// <param name="time">New time</param>
+        /// <returns>A new DateTime that its time is set.</returns>
+        public static DateTime SetTime(this DateTime baseDate, TimeSpan time) => baseDate.Date.Add(time);
+
+        public static DateTimeOffset SetTime(this DateTimeOffset baseDate, TimeSpan time) =>
+            new(baseDate.Year, baseDate.Month, baseDate.Day, time.Hours, time.Minutes, time.Seconds, baseDate.Offset);
+
+        public static DateTimeOffset AddTimeZone(this DateTime baseDate, TimeSpan offset) => new(baseDate.Ticks, offset);
+
+        public static DateTimeOffset ConvertByTimeZone(this DateTime baseDate, string targetZone) =>
+            ConvertByTimeZone(baseDate, TimeSpan.Zero, targetZone);
+
+        public static DateTimeOffset ConvertByTimeZone(this DateTime baseDate, TimeSpan offset, string targetZone)
+        {
+            TimeZoneInfo zone = TZConvert.GetTimeZoneInfo(targetZone);
+            var targetOffset = zone.GetUtcOffset(baseDate);
+            var d = DateTime.SpecifyKind(baseDate, DateTimeKind.Unspecified);
+            return new DateTimeOffset(d.Add(targetOffset).Add(offset), targetOffset);
+        }
+
+        public static DateTimeOffset ConvertByTimeZone(this DateTimeOffset baseDate, string targetZone) =>
+            ConvertByTimeZone(baseDate.UtcDateTime, TimeSpan.Zero, targetZone);
+
+        /// <summary>
+        /// Gets the time zone offset with is different from UTC
+        /// </summary>
+        /// <param name="zone">Standard form of time zone</param>
+        /// <param name="respectDayLightSaving">If true, the default time zone offset is returned and the daylight saving
+        /// is considered. If false is passed, the default base time zone is returned and the daylight saving
+        /// is not considered</param>
+        /// <param name="preferredDate">The date in which the daylight is checked. The default value will be DateTime.UtcNow</param>
+        /// <returns></returns>
+        public static TimeSpan GetTimeZoneOffset(string zone, bool respectDayLightSaving = true, DateTime preferredDate = default)
+        {
+            var targetDate = preferredDate == default ? DateTime.UtcNow : preferredDate;
+            return respectDayLightSaving 
+                ? TZConvert.GetTimeZoneInfo(zone).GetUtcOffset(targetDate)
+                : TZConvert.GetTimeZoneInfo(zone).BaseUtcOffset;
+        }
+
+        /// <summary>
+        /// The offset between two time zones are calculated
+        /// </summary>
+        /// <example>The difference between Iran Standard time and UTC is 3:30 hours.</example>
+        /// <code>GetTimeZoneOffset("Iran Standard time", "UTC") = 3:30</code>
+        /// <param name="sourceZone"></param>
+        /// <param name="destinationZone"></param>
+        /// <returns></returns>
+        public static TimeSpan GetTimeZoneOffset(string sourceZone, string destinationZone) =>
+            GetTimeZoneOffset(sourceZone) - GetTimeZoneOffset(destinationZone);
+    }
+}
---
+++ DateExtensionsTest.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using Doctors.Service.Gen.ViewModels.Output;
+using Doctors.Service.Models;
+using Doctors.Service.ViewModels.Output;
+using TimeZoneConverter;
+using Xunit;
+
+namespace Doctors.Service.Extensions
+{
+    public class DateExtensionsTest
+    {
+        private const string Zone = "Pacific Standard Time";
+        private const string DateWithDayLightSaving = "2022-03-13 03:00";
+        private const string DateWithoutDayLightSaving = "2022-11-06 03:00";
+
+        [Theory]
+        [InlineData(Zone, DateWithoutDayLightSaving, "-08:00")]
+        [InlineData(Zone, DateWithDayLightSaving,    "-07:00")]
+        public void ShouldGetTimeZoneOffset_RespectDayLightSaving(string zone, string dateTime, string offset)
+        {
+            var date = DateTime.Parse(dateTime);
+            var expected = TimeSpan.Parse(offset);
+
+            var actual = TZConvert.GetTimeZoneInfo(zone).GetUtcOffset(date);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ShouldChangeDateTimeOffset()
+        {
+            var dateTime = DateTime.Parse(DateWithoutDayLightSaving);
+
+            var irvineTime = dateTime.ConvertByTimeZone(Zone);
+            var actual = irvineTime.DateTime.Subtract(dateTime);
+            var expected = TimeSpan.Parse("-08:00");
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ShouldSetTimeForDateTime()
+        {
+            var today = DateTime.Now;
+            var time = TimeSpan.Parse("08:00");
+
+            var combined = today.SetTime(time);
+            Assert.True(TimeSpan.Equals(time, combined.TimeOfDay));
+        }
+
+        [Fact]
+        public void ShouldSetTimeForDateTimeOffset()
+        {
+            var today = DateTimeOffset.Now;
+            var time = TimeSpan.Parse("08:00");
+
+            var combined = today.SetTime(time);
+            Assert.True(TimeSpan.Equals(time, combined.TimeOfDay));
+        }
+        
+        [Fact]
+        public void ShouldGetTimeOffsetOfZone()
+        {
+            var tz = "Asia/Tehran";
+            var expectedOffset = new TimeSpan(3, 30, 0);
+            var actual = DateExtensions.GetTimeZoneOffset(tz, false);
+
+            Assert.Equal(actual, expectedOffset);
+        }
+
+        [Theory]
+        [InlineData("UTC", "00:00:00")]
+        [InlineData("Pacific Standard Time", "-8:00")]
+        [InlineData("Central Standard Time", "-6:00")]
+        public void Test_GetTimeZoneOffset(string zone, string offsetToUtc)
+        {
+            var actual = DateExtensions.GetTimeZoneOffset(zone, false);
+            var expected = TimeSpan.Parse(offsetToUtc);
+
+            Assert.Equal(actual, expected);
+        }
+
+        
+        /// <summary>
+        /// The test is used to 
+        /// </summary>
+        [Fact]
+        public void Test_Convert_Extension_Method()
+        {
+            var date1 = DateTimeOffset.Parse("3/10/2022 22:00:00 -08:00");
+            var startTime = new TimeSpan(18, 30, 0);
+            var duration = TimeSpan.FromMinutes(30);
+            var endTime = startTime + duration;
+
+            var flatAvailableDate = new FlatAvailableDateModel()
+            {
+                Date = date1,
+                DoctorOffice = new ClinicOutputViewModel() {ClinicId = 1},
+                FreeTime = new FreeTimeOutputViewModel()
+                {
+                    AvailableTime = startTime,
+                    EndTime = endTime
+                }
+            };
+
+            new List<FlatAvailableDateModel>() {flatAvailableDate}
+                .ConvertTo("Pacific Standard Time", "Iran Standard Time");
+
+
+            var expectedStartTime = new TimeSpan(30, 0, 0);
+            var expectedEndTime = expectedStartTime + duration;
+
+            Assert.Equal(expectedStartTime.Hours, flatAvailableDate.FreeTime.AvailableTime.Hours);
+            Assert.Equal(expectedEndTime.Hours, flatAvailableDate.FreeTime.EndTime.Hours);
+
+            if (!DateTime.Equals(date1.Date, flatAvailableDate.Date.Date))
+                Assert.Equal(1, Math.Abs(date1.Date.Subtract(flatAvailableDate.Date.Date).Days));
+        }
+
+        [Theory]
+        [InlineData("Iran Standard Time", "UTC", "03:30")]
+        [InlineData("UTC", "Iran Standard Time", "-3:30")]
+        [InlineData("Central Standard Time", "UTC", "-5:00")]
+        [InlineData("Central Standard Time", "Pacific Standard Time", "02:00")]
+        public void ShouldCalculateTimeZoneDifferenceBetweenTwoZones(string sourceZone, string targetZone, string difference)
+        {
+            var expectedOffset = TimeSpan.Parse(difference);
+            var actualOffset = DateExtensions.GetTimeZoneOffset(sourceZone, targetZone);
+            Assert.Equal(expectedOffset, actualOffset);
+        }
+    }
+}
